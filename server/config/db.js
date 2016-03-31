7063b3cd5afad539b5cc05dcc071dde689631c00
p1brighto <p1brighto@gmail.com>
2016-03-31 11:51:37 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'url': 'mongodb://p1brighto:chirackal1@ds015929.mlab.com:15929/week9-email-send'
+};
+
+//# sourceMappingURL=db.js.map
---
+++ server/models/user.js
@@ -0,0 +1,41 @@
+"use strict";
+var mongoose = require('mongoose');
+// DEFINE THE OBJECT SCHEMA
+var userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'username is required'
+    },
+    password: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'password is required'
+    },
+    email: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'email is required'
+    },
+    displayName: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Display Name is required'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date,
+        default: Date.now
+    }
+}, { collection: 'userInfo' });
+// MAKE THIS PUBLIC SO THE CONTROLLER CAN SEE IT
+exports.User = mongoose.model('User', userSchema);
+
+//# sourceMappingURL=user.js.map
